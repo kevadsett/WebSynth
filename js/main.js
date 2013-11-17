ea03c3d77f5df3f015e41e94f4c39911dfd0a801
Kev Adsett <kev.adsett@gmail.com>
2013-11-17 16:36:07 +0000

    Adding global properties to WebSynth object

--- js/main.js
+++ js/main.js
@@ -1,5 +1,9 @@
-var NOTE_NAMES = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"];
+WebSynth.NOTE_NAMES = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"];
+WebSynth.BOTTOM_C_FREQ = 8.1757989156; // hertz
+WebSynth.WebAudio = new WebAudioController();
+WebSynth.NoteConverter = new NoteConverter();
 
 $(document).on('ready', function() {
-    console.log(new OscModule());
+    window.osc = new OscModule({context: WebSynth.WebAudio.get('context')});
+    console.log(osc);
 });
