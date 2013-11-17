80c63b98217379e803dc8d0c9b54c580516d9f3d
Kev Adsett <kev.adsett@gmail.com>
2013-11-17 16:23:32 +0000

    renaming NoteConverter

--- js/controllers/NoteConverter.js
+++ js/controllers/NoteConverter.js
@@ -1,12 +1,12 @@
-var NoteController = {
+var NoteConverter = Backbone.Model.extend({
     getNoteNumberFromName: function(noteName) {
         var octaveNumber = parseInt(noteName.match(/\d+/g)),
-            noteNum = _.indexOf(NOTE_NAMES, noteName.match(/[^\d+]/g).join(""));
+            noteNum = _.indexOf(WebSynth.NOTE_NAMES, noteName.match(/[^\d+]/g).join(""));
         return noteNum + (12 * octaveNumber);
     },
     
     getNoteNameFromNumber: function(noteNumber) {
-        var noteName = NOTE_NAMES[noteNumber % 12]
+        var noteName = WebSynth.NOTE_NAMES[noteNumber % 12]
         noteName += Math.floor(noteNumber / 12);
         return noteName;
     },
@@ -16,8 +16,6 @@ var NoteController = {
     },
     
     getFrequencyFromNoteNumber: function(noteNumber) {
-        var bottomC = 8.1757989156;
-        return Math.pow(2, noteNumber/12) * bottomC;
+        return Math.pow(2, noteNumber/12) * WebSynth.BOTTOM_C_FREQ;
     },
-    
-};
+});
