3d1d94ab417ac29a9840afefe69475ad7f07911f
Kev Adsett <kev.adsett@gmail.com>
2013-11-17 16:29:57 +0000

    Creating setters and getters for LineModule

--- js/models/LineModule.js
+++ js/models/LineModule.js
@@ -6,5 +6,21 @@ var LineModule = BaseModule.extend({
         }
     },
     initialize: function() {
+        if(this.get('context') !== undefined) {
+            this.set('output', this.get('context').createGain());
+            this.get('output').gain.value = 0;
+        }
+    },
+    setToMaxValue: function() {
+        this.get('output').gain.value = this.get('range').hi;
+    },
+    setToMinValue: function() {
+        this.get('output').gain.value = this.get('range').lo;
+    },
+    getValue: function() {
+        return mapValue(this.get('output').gain.value, 0, 1, this.get('range').lo, this.get('range').hi);
+    },
+    setValue: function(newValue) {
+        this.get('output').gain.value = normalise(newValue, this.get('range').lo, this.get('range').hi);
     }
 });
