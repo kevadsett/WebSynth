6bc69931ecf2967ca45ae577df123116812e665c
Kev Adsett <kev.adsett@gmail.com>
2013-11-17 16:31:22 +0000

    Adding convenience methods in EnumModule

--- js/models/EnumModule.js
+++ js/models/EnumModule.js
@@ -2,5 +2,11 @@ var EnumModule = StepModule.extend({
     initialize: function() {
         StepModule.prototype.initialize.apply(this);
         this.set('numberOfSteps', this.get('enum').length);
+    },
+    getIndexOf: function(str) {
+        return _.indexOf(this.get('enum'), str);
+    },
+    getEnum: function(index) {
+        return this.get('enum')[index];
     }
 });
